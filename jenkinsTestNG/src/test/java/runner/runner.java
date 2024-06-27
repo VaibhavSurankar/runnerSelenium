9b4fd8b0cecbd256f79523a75dde74c6ceb6bc5d
Vaibhav Surankar <vsurankar416@gmail.com>
2024-06-27 08:00:58 +0530

    runnerSelenium

---
+++ jenkinsTestNG/src/test/java/runner/runner.java
@@ -0,0 +1,37 @@
+package runner;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class runner {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        // Initialize WebDriver
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test(priority = 1)
+    public void GoogleTest() {
+        driver.get("https://google.com");
+        System.out.println("Title:" + driver.getTitle());
+    }
+
+    @AfterTest
+    public void tearDown() {
+        // Quit WebDriver
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
