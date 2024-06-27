441fa26cb3f9b67840e213f50afbaedcc1f9a845
Vaibhav Surankar <vsurankar416@gmail.com>
2024-06-27 08:21:49 +0530

    new

--- jenkinsTestNG/src/test/java/runner/runner.java
+++ jenkinsTestNG/src/test/java/runner/runner.java
@@ -1,23 +1,28 @@
 package runner;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class runner {
     WebDriver driver;
 
     @BeforeTest
     public void setUp() {
-        // Initialize WebDriver
-    	System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Desktop\\Seed Tution\\Setup\\chromedriver-win64\\chromedriver.exe");
-        driver.manage().window().maximize();
+        // Set path to chromedriver executable
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Desktop\\Seed Tution\\Setup\\chromedriver-win64\\chromedriver.exe");
+
+        // Configure Chrome options
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized"); // Maximize browser window on startup
+
+        // Initialize ChromeDriver
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
